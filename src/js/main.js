91e9c1f354b9fce7a7908543f0b68fffbb00c7bf
alterman007 <1205823081@qq.com>
2016-08-28 15:42:40 +0800

    done

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$(".owl-carousel").owlCarousel({
+		items:1,
+		loop:true,
+		// nav:true,
+		autoplayHoverPause:true,
+		autoplay:true,
+		autoplayTimeout:3000
+	});
+});
